8c2e8d43447be646f4d2a11525209ea451e226dd
rjrichiRich <jaiswalricha32@gmail.com>
2024-08-03 12:05:12 +0530

    Adds logic for finding Even and Odd number

---
+++ src/main/java/com/rj/FindOddorEven.java
@@ -0,0 +1,26 @@
+package com.rj;
+
+public class FindOddorEven {
+
+    public static void main(String[] args) {
+        int num = 2;
+        System.out.println(oddEven(num));
+    }
+
+    public static String oddEven(int num){
+        String message;
+        if(num ==2 || num ==0){
+             message = "Number is Even";
+            return message;
+        }
+        else if (num%2==0){
+            message ="Number is Even";
+            return message;
+        }
+        else {
+            message = "Number is Odd";
+            return message;
+        }
+    }
+}
+
