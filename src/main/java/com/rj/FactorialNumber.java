58f65d1976ef79dc139c784c4feccb9c1dfee2b5
rjrichiRich <jaiswalricha32@gmail.com>
2024-08-03 13:22:36 +0530

    Adds logic for Factorial Number

---
+++ src/main/java/com/rj/FactorialNumber.java
@@ -0,0 +1,29 @@
+package com.rj;
+
+public class FactorialNumber {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Factorial of number : " + factorialOfNumber(num));
+        System.out.println("Factorial of number(2) : " + factorialNumber(num));
+    }
+
+    public static int factorialOfNumber(int num) {
+        int fact = 0;
+        int tmp = num;
+
+        for (int i = num - 1; i < num && i > 0; i--) {
+            fact = tmp * (i);
+            tmp = fact;
+        }
+
+        return fact;
+    }
+
+    public static int factorialNumber(int num){
+        int factorial =1;
+        for (int i =1; i<=num; i++){
+            factorial = factorial*i;
+        }
+        return factorial;
+    }
+}
