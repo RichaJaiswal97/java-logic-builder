d3afb03616f95d9c9ce18ad9f926a97c9a75947b
rjrichiRich <jaiswalricha32@gmail.com>
2024-08-03 12:36:17 +0530

    Adds logic for Fibonacci Series

---
+++ src/main/java/com/rj/FibonacciSeries.java
@@ -0,0 +1,24 @@
+package com.rj;
+
+public class FibonacciSeries {
+
+    public static void main(String[] args) {
+        int num = 9;
+        fibonacciSeries(num);
+    }
+
+    public static void fibonacciSeries(int num) {
+        int n1 = 1;
+        int n2 = 2;
+        int n3;
+
+        System.out.print(n1 + ", " + n2);
+        for (int i = 2; i < num; i++) {
+            n3 = n1 + n2;
+            System.out.print(", " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+
+    }
+}
