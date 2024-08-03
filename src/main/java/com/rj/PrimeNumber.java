998b5d4b220b9128166f1f630acd4712be6f74cb
rjrichiRich <jaiswalricha32@gmail.com>
2024-08-03 12:27:21 +0530

    Adds logic for prime number

---
+++ src/main/java/com/rj/PrimeNumber.java
@@ -0,0 +1,22 @@
+package com.rj;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int num = 113;
+        System.out.println(isPrimeNumber(num));
+    }
+    public static boolean isPrimeNumber(int num){
+        int temp = num;
+        int i = 2;
+        if(temp==2){
+            return true;
+        }
+        while (num>i){
+              if(temp%i==0){
+                return false;
+            }
+            i++;
+              }
+        return true;
+    }
+}
